cea9353c334463fb923c3a20ed2a497813f19f3b
davigomessales <davi.gomes.sales@gmail.com>
2023-01-03 19:01:44 -0300

    adicionando functions

---
+++ Funções/ArrowFunctions.js
@@ -0,0 +1,9 @@
+// Altere plantaPrecisaDeAgua para usar a sintaxe de arrow function
+
+const plantaPrecisaDeAgua = function(dia) {
+    if (dia === 'Quarta-feira') {
+      return true;
+    } else {
+      return false;
+    }
+  };
