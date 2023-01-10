36f781869f8d60b7dfe1d75317fed8490a195b3f
davigomessales <davi.gomes.sales@gmail.com>
2023-01-10 18:47:55 -0300

    adding arrays

---
+++ Arrays/ArrayMethods.js
@@ -0,0 +1,37 @@
+
+//Metodos de Array
+// Push
+//#region
+    // 1 - Adicione dois elementos para a lista de tarefas.
+    const tarefas1 = ['lavar pratos', 'lavar roupas', 'botar o lixo para fora'];
+//#endregion
+
+// Pull
+//#region
+    // 1 - Remova o ultimo elemento da lista de tarefas.
+    const tarefas2 = ['lavar pratos', 'lavar roupas', 'botar o lixo para fora', 'fazer o jantar', 'passar pano na casa'];
+//#endregion
+
+// Shift/Unshift/Slice
+//#region
+//Considere o array abaixo
+const listaDeCompras2 = ['suco de laranja', 'bananas', 'café', 'arroz', 'macarrão', 'óleo de soja', 'vegetais'];
+
+// 1 - Use o método .shift() para remover o primeiro item do array listaDeCompras2.
+// 2 - Exiba o novo array listaDeCompras2 no console.
+// 3 - Use o método .unshift() para adicionar 'pipoca' ao início de sua lista de compras.
+// 4 - Você pode excluir a instrução console.log() da etapa anterior.
+// 5 - Você está com pressa, então decide pedir a um amigo para ajudá-lo com suas compras.
+//     Você quer que ele pegue as 'bananas', 'café' e 'arroz'.
+
+// 6 - No código que você adicionou na questão, use .slice() para fornecer a seu amigo uma lista
+// dessas três coisas.
+
+// 7 - Exiba o valor do  de listaDeCompras2, perceba que a lista original não foi alterada, o que demonstra que o 
+// método .slice é non-mutating.
+
+// 8 - Vamos encontrar o índice de um elemento específico em listaDeCompras2 usando o .indexOf()
+//  Chame .indexOf() em listaDeCompras2 para encontrar o índice do elemento 'arroz' e
+//  salve o valor retornado em uma variável const chamada arrozIndex.
+
+//#endregion
---
+++ Arrays/Arrays.js
@@ -0,0 +1,70 @@
+// Criando Arrays
+    //#region 
+    // 1. Declare uma variável usando const chamada hobbies e defina como um array com tres strings dentro.
+    // Use console.log() para imprimir no console.
+    const hobbies = [42, 'praticar falcoaria', 'Falar com patos']
+    console.log(hobbies)
+    console.log(hobbies[0])
+
+   //#endregion
+      
+// Acessando elementos
+   //#region 
+
+   
+   
+   // Considere o array abaixo para as próximas 3 atividades:
+
+   const citacoesFamosas = ['Você não pode ensinar nada a ninguém, mas pode ajudar as pessoas a descobrirem por si mesmas.',
+   'Em algum lugar, alguma coisa incrível está esperando para ser descoberta.',
+   'Nós somos o que fazemos repetidamente. A excelência, então, não é um ato, mas um hábito.'];
+
+    //   1 - Elementos individuais em arrays também podem ser armazenados em variáveis.
+    //       Crie uma variável nomeada listItem e defina-a igual ao primeiro item no
+    //       array citacoesFamosas usando a notação de colchetes ([]).
+    //       Em seguida, use console.log() para imprimir a variável listItem no console.
+
+    //   2 - Agora, use o console.log() no terceiro elemento no array citacoesFamosas usando a notação de colchetes para acessar o elemento.
+    //       Não salve o elemento em uma nova variável antes de exibir na tela.
+
+    //   3 - Você pode acessar cada elemento em um array usando o índice. Mas o que acontece se você
+    //       tentar acessar um índice que está além do último elemento? Tente exibir no console o
+    //       item no índice [3] de citacoesFamosas. O que aparece?
+   //#endregion
+
+// Atualizando elementos
+    //#region 
+// Considere o seguinte array:
+
+        let listaDeCompras = ['pão', 'tomates', 'leite'];
+
+// 1. Altere o segundo elemento do array listaDeCompras para 'abacates'
+//#endregion
+
+// Arrays com let e const
+//#region 
+// Considere os seguintes Arrays
+
+let condimentos = ['Ketchup',
+                   'Mostarda',
+                   'Molho de Soja',
+                   'Maionese'];
+
+const utensilios = ['Garfo',
+                    'Faca',
+                    'Pauzinhos',
+                    'Garfo'];
+
+        // 1 - Abaixo dos dois arrays existentes,
+        // reatribua o elemento de condimentos no índice 0 para 'Pimenta'.                    
+        // Exiba o array condimentos atualizado no console.
+        // 2 - Abaixo do seu código da Etapa 1, reatribua condimentos para ser um novo array que contém uma única string['Pimenta']
+        // 3 - Abaixo do código da Etapa 2, reatribua o último item do array utensílios para 'Colher'.
+//#endregion
+
+//Tamanho/Length
+//#region 
+
+    // Encontre o tamanho do array objetivos e exiba no console.
+    const objetivos = ['Aprender um novo idioma', 'Ler 52 livros', 'Correr uma maratona'];
+//#endregion
